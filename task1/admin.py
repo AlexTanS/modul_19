e3314d68042e29d4392cb065e28f0b0b2376dbae
test <60185787+AlexTanS@users.noreply.github.com>
2024-12-22 20:00:01 +0300

    module 19 4 Admin Panel

--- task1/admin.py
+++ task1/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Buyer, Game
 
-# Register your models here.
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_filter = ("size", "cost",)  # фильтрация
+    list_display = ("title", "cost", "size",)  # поля отображения списком
+    search_fields = ("title",)  # поле для поиска
+    list_per_page = 20  # ограничение количества записей на странице
+
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_filter = ("balance", "age",)
+    list_display = ("name", "balance", "age")
+    search_fields = ("name",)
+    list_per_page = 30
+    readonly_fields = ("balance",)  # только для чтения
